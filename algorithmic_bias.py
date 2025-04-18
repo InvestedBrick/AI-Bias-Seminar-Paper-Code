ae3e4beb03fee0388b68e64fa52f2fbebc40c8eb
InvestedBrick <julianbrecht25@gmail.com>
2025-04-18 15:24:16 +0200

    updated

--- algorithmic_bias.py
+++ algorithmic_bias.py
@@ -1,25 +1,26 @@
 # A demonstration for algorithmic bias in a simple neural network model
-# REWORK, may not be correctly algorithmic bias
 import keras
 import numpy as np
 
 class Person:
-    def __init__(self,education_level,employed,postal_code):
-        self.education_level = education_level
+    def __init__(self,credit_history,income,employed,postal_code):
+        self.credit_history = credit_history
         self.employed = employed
+        self.income = income * employed
         self.postal_code = postal_code
 
         #supposed simulate how getting a loan may depend on where you live (high income areas vs rather poor areas)
-        self.would_get_loan = education_level * 1.5 +  employed * 5 + postal_code/100 * 3 > 25
+        self.would_get_loan = self.income * 0.0003 * employed + credit_history * 3 + postal_code * 0.1 > 85
     
 def create_people(num_people):
     people = []
 
     for _ in range(num_people):
-        education_level = np.random.randint(0, 5)
+        credit_history = np.random.randint(0, 5)
         employed = np.random.randint(0, 2)
+        income = np.random.randint(0, 100000)
         postal_code = np.random.randint(0, 1000)
-        person = Person(education_level, employed, postal_code)
+        person = Person(credit_history,income, employed, postal_code)
         people.append(person)
     
     return people
@@ -29,12 +30,12 @@ def main():
     people = create_people(10000)
 
     # Select sample
-    sample = [p for p in people if np.random.rand() > 0.4] # we take a random subpopulation
-    X = np.array([[p.education_level, p.employed, p.postal_code] for p in sample])
+    sample = [p for p in people if np.random.rand() > 0.7] # we take a random subpopulation
+    X = np.array([[p.credit_history, p.employed, p.income,p.postal_code] for p in sample])
     y = np.array([p.would_get_loan for p in sample])
 
     model = keras.Sequential([
-        keras.layers.InputLayer(input_shape=(3,)),
+        keras.layers.InputLayer(input_shape=(4,)),
         keras.layers.Dense(12, activation='relu'),
         keras.layers.Dense(12, activation='relu'),
         keras.layers.Dense(1, activation='sigmoid')
@@ -43,13 +44,13 @@ def main():
 
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-    model.fit(X, y, epochs=50, batch_size=32)
+    model.fit(X, y, epochs=200, batch_size=32)
 
 
     # Test the model on example data
 
-    print(f"Model input: [education=3,employed=1,postal_code=200], Should they get the load according to the model?: {model.predict(np.array([[3,1,200]]))[0][0] > 0.5}" )
-    print(f"Model input: [education=3,employed=1,postal_code=950], Should they get the load according to the model?: {model.predict(np.array([[3,1,950]]))[0][0] > 0.5}" )
+    print(f"Model input: [credit_history=2,employed=1,income=50_000,postal_code=200], Should they get the load according to the model?: {model.predict(np.array([[2,1,50000,200]]))[0][0] > 0.5}" )
+    print(f"Model input: [credit_history=2,employed=1,income=50_000,postal_code=950], Should they get the load according to the model?: {model.predict(np.array([[2,1,50000,950]]))[0][0] > 0.5}" )
 
 
 
